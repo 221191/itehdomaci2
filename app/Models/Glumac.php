5c21df444285cce5a2ff39ed5b99e59dcd6924b7
Nemanja <nemanjavasilev@gmail.com>
2022-12-11 01:32:54 +0100

    modeli i veze

---
+++ app/Models/Glumac.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+// veza sa predstavom
+use App\Models\Predstava;
+
+
+class Glumac extends Model
+{
+    use HasFactory;
+
+    public function predstave(){
+        return $this->hasMany(Predstava::class);
+    }
+}
---
+++ app/Models/Predstava.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+// veza sa rediteljem
+use App\Models\Reditelj;
+// veza sa glumcem
+use App\Models\Glumac;
+class Predstava extends Model
+{
+    use HasFactory;
+
+    public function reditelj(){
+        return $this->belongsTo(Reditelj::class);
+    }
+
+    public function glumci(){
+        return $this->hasMany(Glumac::class);
+    }
+}
