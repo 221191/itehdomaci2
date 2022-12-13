317614d4123b83bdadad6c890ba58ede712830fe
Nemanja <nemanjavasilev@gmail.com>
2022-12-13 15:47:52 +0100

    migracije za kreiranje novih tabela

---
+++ database/migrations/2022_12_13_142800_create_rediteljs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRediteljsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rediteljs', function (Blueprint $table) {
+            $table->id();
+            $table->string('imeReditelja');
+            $table->string('prezimeReditelja');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rediteljs');
+    }
+}
---
+++ database/migrations/2022_12_13_144309_create_predstavas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePredstavasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('predstavas', function (Blueprint $table) {
+            $table->id();
+            $table->string('sifraPredstave')->unique();
+            $table->string('imePredstave');
+            $table->double('cenaKarte',8,2);
+            $table->foreignId('reditelj_id')->constrained('rediteljs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('predstavas');
+    }
+}
