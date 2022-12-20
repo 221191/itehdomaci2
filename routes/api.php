e2bee1d79a03e5fdeeb9672fe1ac1862c4ec74e6
Nemanja <nemanjavasilev@gmail.com>
2022-12-20 14:10:39 +0100

    autentifikovan korisnik za manipulaciju sa bazom

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RediteljController;
 use App\Http\Controllers\GlumacController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,21 +15,28 @@ use App\Http\Controllers\GlumacController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-// Za reditelja
-// vraca sve reditelje
-Route::get('reditelj', [RediteljController::class, 'index']);
-// vraca reditelja sa prosledjenim paramterom(id-ijem) 
-Route::get('reditelj/{reditelj}', [RediteljController::class, 'show']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
 // azurira se reditelj sa prosledjenim paramterom(id-ijem)
 Route::put('reditelj/{reditelj}', [RediteljController::class, 'update']);
 // brise reditelja
 Route::delete('reditelj/{reditelj}', [RediteljController::class, 'destroy']);
 
+
+// brisanje glumca
+Route::delete('glumac/{glumac}', [GlumacController::class, 'destroy']);
+
+Route::post('logout',[AuthController::class, 'logout']);
+});
+// Za reditelja
+// vraca sve reditelje
+Route::get('reditelj', [RediteljController::class, 'index']);
+// vraca reditelja sa prosledjenim paramterom(id-ijem) 
+Route::get('reditelj/{reditelj}', [RediteljController::class, 'show']);
 // Za glumca
 // vraca sve glumce
 Route::get('glumac', [GlumacController::class, 'index']);
-// brisanje glumca
-Route::delete('glumac/{glumac}', [GlumacController::class, 'destroy']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Autentifikacija korisnika
+Route::post('register',[AuthController::class, 'register']);
+Route::post('login',[AuthController::class, 'login']);
